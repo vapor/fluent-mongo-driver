7143e542d6a50796d6c3f55d8ba7fd796f7547ed
Joannis Orlandos <joannis@orlandos.nl>
2020-02-18 21:58:08 +0100

    Ignore swiftPM and fix the package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,22 +9,20 @@ let package = Package(
        .macOS(.v10_14)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "FluentMongoDriver",
             targets: ["FluentMongoDriver"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vapor/fluent-kit.git", .revision("tn-field-key")),
+        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("tn-field-key")),
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "6.0.0"),
     ],
     targets: [
         .target(
             name: "FluentMongoDriver",
             dependencies: ["FluentKit", "MongoKitten"]),
-//        .testTarget(
-//            name: "FluentMongoDriverTests",
-//            dependencies: ["FluentMongoDriver", "FluentBenchmark"]),
+        .testTarget(
+            name: "FluentMongoDriverTests",
+            dependencies: ["FluentMongoDriver", "FluentBenchmark"]),
     ]
 )
