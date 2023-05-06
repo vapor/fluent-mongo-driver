61296615711a5be4c227ade633531a28a82ffe93
Gwynne Raskind <gwynne@vapor.codes>
2023-05-06 03:44:09 -0500

    Update min Swift version and platforms requirements (#57)
    * Update minimum Swift version to 5.6 and make platform versions consistent.
    
    * CI and exports cleanup

--- Sources/FluentMongoDriver/Exports.swift
+++ Sources/FluentMongoDriver/Exports.swift
@@ -1,4 +1,11 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import struct BSON.ObjectId
+@_documentation(visibility: internal) @_exported import struct MongoKitten.GridFSFile
+@_documentation(visibility: internal) @_exported import struct BSON.Document
+@_documentation(visibility: internal) @_exported import protocol BSON.Primitive
+
+#else
 
 @_exported import struct BSON.ObjectId
 @_exported import struct MongoKitten.GridFSFile
